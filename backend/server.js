a78b44a2f75cf7c9079e11b4e82daff07f9b4605
Sahil Bakshi <sahilbakshi3@gmail.com>
2026-04-05 19:28:28 +0530

    drawing events, sticky notes, undo+ clear

--- backend/server.js
+++ backend/server.js
@@ -23,6 +23,10 @@ const COLORS = [
 ];
 
 let colorIndex = 0;
+let canvasState = {
+  strokes: [],
+  stickies: [],
+};
 
 io.on("connection", (socket) => {
   const userColor = COLORS[colorIndex % COLORS.length];
@@ -63,6 +67,57 @@ io.on("connection", (socket) => {
         name: u.name,
       });
     }
+  });
+
+  socket.on("draw-start", (data) => {
+    socket.broadcast.emit("draw-start", { ...data, userId: socket.id });
+  });
+
+  socket.on("draw", (data) => {
+    socket.broadcast.emit("draw", { ...data, userId: socket.id });
+  });
+
+  socket.on("draw-end", (stroke) => {
+    canvasState.stroke.push(stroke);
+    socket.broadcast.emit("draw-end", { ...stroke, userId: socket.id });
+  });
+
+  socket.on("add-sticky", (sticky) => {
+    const newSticky = {
+      ...sticky,
+      id: `sticky-${Date.now()}-${socket.id}`,
+      color: users.get(socket.id)?.color || "#FFEAA7",
+    };
+
+    canvasState.stickies.push(newSticky);
+    io.emit("sticky-added", newSticky);
+  });
+
+  socket.on("update-sticky", (data) => {
+    const sticky = canvasState.stickies.find((s) => s.id === data.id);
+    if (sticky) sticky.text = data.text;
+    socket.broadcast.emit("sticky-updated", data);
+  });
+
+  socket.on("move-sticky", (data) => {
+    const sticky = canvasState.stickies.find((s) => s.id === data.id);
+    if (sticky) {
+      sticky.x = data.x;
+      sticky.y = data.y;
+    }
+    socket.broadcast.emit("sticky-moved", data);
+  });
+
+  socket.on("undo", () => {
+    if (canvasState.strokes.length > 0) {
+      canvasState.strokes.pop();
+      io.emit("canvas-state", canvasState);
+    }
+  });
+
+  socket.on("clear-canvas", () => {
+    canvasState = { strokes: [], stickies: [] };
+    io.emit("canvas-cleared");
   });
 
   socket.on("disconnect", () => {
